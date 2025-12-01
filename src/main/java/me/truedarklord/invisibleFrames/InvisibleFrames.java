0789dc7edfc8c0a171ee33925a071bfdf32ecd3a
TrueDarkLord <truedarklord.email@gmail.com>
2025-11-30 19:35:43 -0700

    Advertise

--- src/main/java/me/truedarklord/invisibleFrames/InvisibleFrames.java
+++ src/main/java/me/truedarklord/invisibleFrames/InvisibleFrames.java
@@ -10,9 +10,39 @@ public final class InvisibleFrames extends JavaPlugin {
     public void onEnable() {
         new Metrics(this, 28184);
 
+        advertise();
+
         new ClickListener(this);
     }
 
     @Override
     public void onDisable() {}
+
+    private void advertise() {
+        this.getServer().getConsoleSender().sendMessage(
+                """
+  
+ §#00AA00================================§#ee2222
+
+ _____            _     _ _     _     \s
+|_   _|          (_)   (_) |   | |    \s
+  | |  _ ____   ___ ___ _| |__ | | ___\s
+  | | | '_ \\ \\ / / / __| | '_ \\| |/ _ \\
+ _| |_| | | \\ V /| \\__ \\ | |_) | |  __/
+|_____|_| |_|\\_/ |_|___/_|_.__/|_|\\___|
+|  ____|                              \s
+| |__ _ __ __ _ _ __ ___   ___  ___   \s
+|  __| '__/ _` | '_ ` _ \\ / _ \\/ __|  \s
+| |  | | | (_| | | | | | |  __/\\__ \\  \s
+|_|  |_|  \\__,_|_| |_| |_|\\___||___/  \s
+
+§#f5da2aBy TrueDarkLord.
+§#00AA00================================
+§#f5da2aFeel free to buy me a coffee:  §#00AA00|
+§bhttps://ko-fi.com/truedarklord §#00AA00|
+§#00AA00================================
+                        """
+        );
+    }
+
 }
