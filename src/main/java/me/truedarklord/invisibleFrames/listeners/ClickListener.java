130bda7ad433c93fe6a30b737daf85c1f8abc4ec
TrueDarkLord <truedarklord.email@gmail.com>
2025-11-30 19:19:05 -0700

    Fix Listener
    Change over to a different event due to item frames bein classified as entities and not blocks.

--- src/main/java/me/truedarklord/invisibleFrames/listeners/ClickListener.java
+++ src/main/java/me/truedarklord/invisibleFrames/listeners/ClickListener.java
@@ -1,14 +1,13 @@
 package me.truedarklord.invisibleFrames.listeners;
 
 import me.truedarklord.invisibleFrames.InvisibleFrames;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
 
 public class ClickListener implements Listener {
 
@@ -17,19 +16,14 @@ public class ClickListener implements Listener {
     }
 
     @EventHandler
-    public void onClick(PlayerInteractEvent event) {
+    public void onClick(PlayerInteractEntityEvent event) {
         Player player = event.getPlayer();
 
-        if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
         if (!player.isSneaking()) return;
 
-        Block block = event.getClickedBlock();
+        if (!(event.getRightClicked() instanceof ItemFrame frame)) return;
 
-        if (block == null) return;
-        if (!block.getType().equals(Material.ITEM_FRAME)) return;
         if (!player.hasPermission("invisibleframes.toggle")) return;
-
-        ItemFrame frame = (ItemFrame) block.getState();
 
         frame.setVisible(!frame.isVisible());
     }
